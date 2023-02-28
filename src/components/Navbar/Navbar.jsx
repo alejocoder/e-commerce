a85ccaa432871634e088e4121e3396fc51f69293
alejandro <alejosamp@gmail.com>
2023-02-28 11:00:06 -0500

    navbar started

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='wrapper'>
+            <div className='left'>
+                <div className='item'>
+                    <img src='../../../public/img/en.png' alt='img'></img>
+                    <KeyboardArrowDownIcon/>
+                </div>
+                <div className='item'>
+                    <span>USD</span>
+                    <KeyboardArrowDownIcon/>
+                </div>
+                <div className='item'>
+                    <Link to='/products/1'> Women</Link>
+                </div>
+                <div className='item'>
+                    <Link to='/products/2'> Men</Link>
+                </div>
+                <div className='item'>
+                    <Link to='/products/3'> Children</Link>
+                </div>
+            </div>
+            <div className='center'>
+                
+            </div>
+            <div className='right'></div>
+        </div>
+    </div>
   )
 }
